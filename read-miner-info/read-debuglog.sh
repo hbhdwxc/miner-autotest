01d8c6e7cc045513a45178a1332b5ee406bf674a
xzx <xuzhenxing@canaan-creative.com>
2018-05-21 14:17:02 +0800

    Update DNA display

--- read-miner-info/read-debuglog.sh
+++ read-miner-info/read-debuglog.sh
@@ -35,7 +35,7 @@ do
     cat $i | sed 's/] /\]\n/g' | grep TMax  | sed 's/TMax\[//g'  | sed 's/\]//g' > $i.TMax
     cat $i | sed 's/] /\]\n/g' | grep WU    | sed 's/WU\[//g'    | sed 's/\]//g' > $i.WU
     cat $i | sed 's/] /\]\n/g' | grep DH    | sed 's/DH\[//g'    | sed 's/\]//g' > $i.DH
-    cat $i | sed 's/] /\]\n/g' | grep DNA   | sed 's/DNA\[//g'   | sed 's/\]//g' > $i.DNA
+    cat $i | sed 's/] /\]\n/g' | grep DNA   | sed 's/DNA\[//g'   | sed 's/\]//g' | cut -b 13- > $i.DNA
 
     # According to WU value, calculate GHSav.
     # Formula: ghsav = WU / 60 * 2^32 /10^9
